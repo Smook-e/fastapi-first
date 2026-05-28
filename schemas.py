e077355a3e4817043256f57eb77b71ef193f3960
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-05-28 09:06:40 +0300

    added schemas

---
+++ schemas.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+    
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    user_id: int
+    user: User
+
+    class Config:
+        orm_mode = True
+
+
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class User(BaseModel):
+    id: int
+    created_at: datetime
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
