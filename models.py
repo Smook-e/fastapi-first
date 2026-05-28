98df67a23821dd76fa0a7dd4cd345df1c8c80b94
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-05-28 09:06:24 +0300

    added models

---
+++ models.py
@@ -0,0 +1,25 @@
+from database import Base
+from sqlalchemy import TIMESTAMP, Column, Integer, String, Boolean, func, text, ForeignKey
+from sqlalchemy.orm import Relationship
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, default=True)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    user = Relationship("User")
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
